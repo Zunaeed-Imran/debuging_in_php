b43ac6c215265bd9e011898d1af29ac65dd4ba63
imran <zunaeede@gmail.com>
2023-11-26 18:17:35 +0600

    main - error handaling using if else condation

---
+++ 1_practice.php
@@ -0,0 +1,14 @@
+<?php
+// Example of error handeling using if else statement
+
+$number = 90;
+
+if($number >= 0){
+  $ex =  2 / $number;
+  echo $ex;
+}else{
+  echo 'Enter a Valid Number';
+}
+
+
+?>
