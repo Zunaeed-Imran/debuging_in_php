7b452d181a183332b3dee672bfba0cb28b996810
imran <zunaeede@gmail.com>
2023-11-27 09:17:39 +0600

    main - try catch example

--- 2_practice.php
+++ 2_practice.php
@@ -1,11 +1,14 @@
 <?php
 
+$n = 0;
+
 try{
-
-}
-
-catch{
-  
+  if($n <= 0)
+  throw new Exception("Enter a valid number");
+$result = 2 / $n;
+echo $result;
+}catch(Exception $e){
+  echo $e->getMessage();
 }
 
 
